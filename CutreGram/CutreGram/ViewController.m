f8f448a067ceeb942941f1df521531a5c4e2aadc
byjuanamn <juan.antonio.martin@gmail.com>
2015-01-19 13:16:49 +0100

    Paso 1: cognito añadido

--- CutreGram/CutreGram/ViewController.m
+++ CutreGram/CutreGram/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+#import "AWSCore.h"
+#import "Cognito.h"
+
 @interface ViewController ()
 
 @end
@@ -17,11 +20,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self warmUpCognitoCredentials];
+    [self storeAndSyncData];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Cognito 
+
+- (void) warmUpCognitoCredentials{
+    
+    AWSCognitoCredentialsProvider *credentialsProvider = [AWSCognitoCredentialsProvider
+                                                          credentialsWithRegionType:AWSRegionEUWest1
+                                                          accountId:@"299724642337"
+                                                          identityPoolId:@"eu-west-1:29df6d38-205c-48f5-8219-94da7d8281fc"
+                                                          unauthRoleArn:@"arn:aws:iam::299724642337:role/Cognito_CursoMBaaSUnauth_DefaultRole"
+                                                          authRoleArn:@"arn:aws:iam::299724642337:role/Cognito_CursoMBaaSAuth_DefaultRole"];
+    
+    AWSServiceConfiguration *configuration = [AWSServiceConfiguration configurationWithRegion:AWSRegionEUWest1
+                                                                          credentialsProvider:credentialsProvider];
+    
+    [AWSServiceManager defaultServiceManager].defaultServiceConfiguration = configuration;
+}
+
+- (void) storeAndSyncData{
+    AWSCognito *syncClient = [AWSCognito defaultCognito];
+    AWSCognitoDataset *dataset = [syncClient openOrCreateDataset:@"myDataset"];
+    [dataset setString:@"ejemplo" forKey:@"Ejemplo"];
+    [dataset synchronize];
 }
 
 @end
